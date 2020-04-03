bd3759b7f144a9db7821d9b6356a26e9eea8d565
joeljames <joeljames1985@gmail.com>
2020-04-03 14:44:59 -0700

    Push up example for 3 partition, 3 broker

--- START_CLUSTER.md
+++ START_CLUSTER.md
@@ -34,13 +34,15 @@
     11. Start the consumer 
         1. bin/kafka-console-consumer.sh  --topic my_topic --from-beginning --bootstrap-server localhost:9092
 
-# Starting Kafka with multiple (3) broker partitions (Kafka is resilient even if a broker goes down when (replication factor is more than 1) you can still publish subscribe)
+# Starting Kafka with 3 broker and 1 partition 
+    Kafka is resilient even if a broker goes down when replication factor is more than 1
+    you can still publish subscribe.
     1. Create 3 separate server-1.properties configuration files
         1. cp server.properties server-1.properties
     2. Change the setting value under  server-{0,1,2}.properties for 
         1. log.dirs to be unique to avoid lock conflicts
         2. broker.id to be unique
-        3. listeners=PLAINTEXT://:9093 to be unique
+        3. listeners=PLAINTEXT://localhost:9093 to be unique
     3. Start zookeeper
         1. bin/zookeeper-server-start.sh config/zookeeper.properties
     4. Start 3 Broker (kafka server)
@@ -59,3 +61,29 @@
         1. Run bin/kafka-topics.sh --describe —topic replicated_topic --zookeeper localhost:2181
         2. You will see Leader changed and Replicas will be 0,1,2 but ISR will be 1,2
         3. You will still be able to publish and consume messages because you still have 2 other brokers running and working
+        
+# Starting Kafka with 3 broker and 3 partition 
+    1. Create 3 separate server-1.properties configuration files
+        1. cp server.properties server-1.properties
+    2. Change the setting value under  server-{0,1,2}.properties for 
+        1. log.dirs to be unique to avoid lock conflicts
+        2. broker.id to be unique
+        3. listeners=PLAINTEXT://localhost:9093 to be unique
+    3. Start zookeeper
+        1. bin/zookeeper-server-start.sh config/zookeeper.properties
+    4. Start 3 Broker (kafka server)
+        1. bin/kafka-server-start.sh config/server-0.properties
+        2. bin/kafka-server-start.sh config/server-1.properties
+        3. bin/kafka-server-start.sh config/server-2.properties
+    5. Create topic 
+        1. bin/kafka-topics.sh --create --topic my-topic-demo --zookeeper localhost:2181 --replication-factor 3 --partitions 3   
+    6. Check details about the topic 
+        1. bin/kafka-topics.sh --describe —topic my-topic-demo --zookeeper localhost:2181   
+        ```bash
+                Topic: my-topic-demo	PartitionCount: 3	ReplicationFactor: 3	Configs:
+                Topic: my-topic-demo	Partition: 0	Leader: 0	Replicas: 0,1,2	Isr: 0,1,2
+                Topic: my-topic-demo	Partition: 1	Leader: 1	Replicas: 1,2,0	Isr: 1,2,0
+                Topic: my-topic-demo	Partition: 2	Leader: 2	Replicas: 2,0,1	Isr: 2,0,1
+        ```   
+    8. Start the consumer 
+        1. bin/kafka-console-consumer.sh  --topic my-topic-demo --bootstrap-server localhost:9092          
--- src/main/java/KafkaProducerApp.java
+++ src/main/java/KafkaProducerApp.java
@@ -8,63 +8,62 @@ import java.util.Properties;
 public class KafkaProducerApp {
 
     public static void main(String[] args) {
+        System.out.println("Starting the producer main application");
         // Set the properties required for the producer
         Properties props = new Properties();
-        props.put("bootstrap.servers", "BROKER-1:9092, BROKER-1:9093");
+        props.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
         //Can only send string messages with StringSerializer, cannot send int float etc
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        KafkaProducer producer = new KafkaProducer(props);
-        String topic = "my_topic";
+        String topic = "my-topic-demo";
 
         // You can also send to a particular partition along with unix timestamp.
         // Not recommended in high scale env as addition bytes have to be sent over via network.
         // Broker logs it's own timestamp
-//        ProducerRecord<Object, String> message1 = new ProducerRecord<>(topic, 1, Instant.now().toEpochMilli(), "Message 1");
-
-        ProducerRecord<Object, String> message1 = new ProducerRecord<>(topic, "key-01", "Message 1");
-        try {
-            producer.send(message1);
-            // When calling the send method the producer will reach out to the cluster using the bootstrap.servers list set above
-            // to discover the cluster membership. The response comes back as metadata, containing info about the
-            // topics, partitions and managing brokers on the cluster.
-            // Producer will use this metatdata instance through out it's life cycle and keep it up to date.
-
-            //Data flow through the pipeline
-            // send message ->
-            // serializer ->
-            // partitioner (decides which partition to send the message) (Strategies: direct, round-robin, key-modhash, custom)
-            // record accumulator (Batches the record which are going to be sent to broker, does not send one at a time. Can be configured in producer properties)
-            // to broker
-            // get back a RecordMetadata
-
-            //Producer can also set the level of acks it should receive from the broker when sending a message
-            //1) 0 -> Fire and forget (fastest not reliable)
-            //2) 1 -> Only the leader broker to ack, instead of all the brokers in the replica to confirm
-            //3) 2 -> all insync replica should confirm (slow performance)
-
-            // When broker responds with error
-            //1) retries
-            //2) retry.backoff.ms // wait period in ms between retries
-
-            // If your application requires message ordering
-            // You can only have ordering if you are sending messages through a single partition
-            // With multiple partations you have to maintain the order at the consumer level. Errors can complicate this too eg retry.backoff.ms.
-            // At a very high performance cost you can set max.inflight.request.per.connection (meaning a producer can only send one message at a time)
-
-            //Message delivery assurance:
-            //1) at most once
-            //2) at least once
-            //3) only once
+        // ProducerRecord<Object, String> message1 = new ProducerRecord<>(topic, 1, Instant.now().toEpochMilli(), "Message 1");
 
 
+        try(KafkaProducer<String, String> producer = new KafkaProducer(props)) {
+            for (int i = 0; i < 150; i++) {
+                // When calling the send method the producer will reach out to the cluster using the bootstrap.servers list set above
+                // to discover the cluster membership. The response comes back as metadata, containing info about the
+                // topics, partitions and managing brokers on the cluster.
+                // Producer will use this metatdata instance through out it's life cycle and keep it up to date.
 
+                //Data flow through the pipeline
+                // send message ->
+                // serializer ->
+                // partitioner (decides which partition to send the message) (Strategies: direct, round-robin, key-modhash, custom)
+                // record accumulator (Batches the record which are going to be sent to broker, does not send one at a time. Can be configured in producer properties)
+                // to broker
+                // get back a RecordMetadata
 
+                //Producer can also set the level of acks it should receive from the broker when sending a message
+                //1) 0 -> Fire and forget (fastest not reliable)
+                //2) 1 -> Only the leader broker to ack, instead of all the brokers in the replica to confirm
+                //3) 2 -> all insync replica should confirm (slow performance)
+
+                // When broker responds with error
+                //1) retries
+                //2) retry.backoff.ms // wait period in ms between retries
+
+                // If your application requires message ordering
+                // You can only have ordering if you are sending messages through a single partition
+                // With multiple partitions you have to maintain the order at the consumer level. Errors can complicate this too eg retry.backoff.ms.
+                // At a very high performance cost you can set max.inflight.request.per.connection (meaning a producer can only send one message at a time)
+
+                //Message delivery assurance:
+                //1) at most once
+                //2) at least once
+                //3) only once
+                System.out.println("Sending message: " + i);
+                ProducerRecord<String, String> message1 = new ProducerRecord<>(topic, "key" + i, "Message " + i);
+                producer.send(message1);
+            }
 
         } catch (Exception e) {
             log.error("Failed to send message by the producer", e);
         }
-
     }
 }
